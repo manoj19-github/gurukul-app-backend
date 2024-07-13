cb650dcb0271fe0ae11c8ec7e5d6c291a2e89c49
Manoj Santra <santramanoj1997@gmail.com>
2024-07-13 21:19:25 +0530

    fixed

--- src/main.ts
+++ src/main.ts
@@ -5,7 +5,7 @@ import { HttpExceptionFilter } from './lib/errorHandler.exception';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
-    origin: 'http://localhost:3000',
+    origin: '*',
   });
   app.useGlobalFilters(new HttpExceptionFilter());
   await app.listen(process.env.PORT ?? 5000);
